e1d1fdc3ee4c8fc7b0322caa2881b8a3faaa6bc4
Sofia Gil <sofia2mil1@gmail.com>
2024-05-27 12:05:55 +0200

    modification receptive field analysis

--- time_to_first_spike_simulation.sh
+++ time_to_first_spike_simulation.sh
@@ -1,3 +1,3 @@
 
 
-run -g 1 -m 24 -t 0:10 -o Out/t2fs.out -e Error/t2fs.err -j t2fs "python time_to_first_spike_simulation.py -seq_len 1250 -delays '1000,0' -n_trials 1"
+run -g 1 -m 24 -t 1:00 -o Out/t2fs.out -e Error/t2fs.err -j t2fs "python time_to_first_spike_simulation.py -seq_len 1250 -delays '1000,0' -n_trials 100"
