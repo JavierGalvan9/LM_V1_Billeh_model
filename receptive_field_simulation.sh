799b44e4a891704dcf2a2a639ccec96740003ec6
Sofia Gil <sofia2mil1@gmail.com>
2024-09-26 12:36:04 +0200

    Update in delete simulation files

--- receptive_field_simulation.sh
+++
@@ -1,3 +0,0 @@
-
-
-run -g 1 -m 24 -t 2:00 -o Out/receptive.out -e Error/receptive.err -j t2fs "python receptive_field_simulation.py -seq_len 350 -delays '100,0' -n_trials 1 -circle_row 0 -circle_column 0 -orientation 0 -radius_circle 10"
